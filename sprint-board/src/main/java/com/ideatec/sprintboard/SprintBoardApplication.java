e0760232ba0837fe5f0e9fb7b11345739941e6fa
mingeon cho <mincoln419@naver.com>
2023-12-21 16:42:08 +0900

    init commit

---
+++ sprint-board/src/main/java/com/ideatec/sprintboard/SprintBoardApplication.java
@@ -0,0 +1,13 @@
+package com.ideatec.sprintboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBoardApplication.class, args);
+	}
+
+}
---
+++ sprint-board/src/test/java/com/ideatec/sprintboard/SprintBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideatec.sprintboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
