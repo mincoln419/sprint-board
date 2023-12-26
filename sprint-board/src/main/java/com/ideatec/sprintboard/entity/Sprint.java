34794c0c9d43e922f1a958f2e8cb28d82814feae
mingeon cho <mincoln419@naver.com>
2023-12-26 17:54:37 +0900

    merge

--- sprint-board/src/main/java/com/ideatec/sprintboard/entity/Sprint.java
+++ sprint-board/src/main/java/com/ideatec/sprintboard/entity/Sprint.java
@@ -1,10 +1,15 @@
 package com.ideatec.sprintboard.entity;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+<<<<<<< Updated upstream
+=======
+import jakarta.persistence.FetchType;
+>>>>>>> Stashed changes
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
@@ -24,18 +29,35 @@ public class Sprint {
 
 	@Id @GeneratedValue
 	private Long id;
-	
+
 	private String title;
+<<<<<<< Updated upstream
 	
 	@OneToOne
 	@JoinColumn(name = "author_id")
 	private SprintUser author;
 	
+=======
+
+	private String author;
+
+>>>>>>> Stashed changes
 	@Enumerated(EnumType.STRING)
 	private SprintType sprintType;
-	
+
+	@Enumerated(EnumType.STRING)
+	private SprintStatus sprintStatus;
+
 	@OneToMany(mappedBy = "sprint")
-	private List<SprintComment> comments; 
-	
-	
+	private List<SprintComment> comments;
+
+	private LocalDateTime beginDtm;
+
+	private LocalDateTime finishDtm;
+
+	private LocalDateTime insertDtm;
+
+	private LocalDateTime updateDtm;
+
+
 }
---
+++ sprint-board/src/main/java/com/ideatec/sprintboard/entity/SprintStatus.java
@@ -0,0 +1,16 @@
+package com.ideatec.sprintboard.entity;
+
+/**
+ * <pre>
+ * com.ideatec.sprintboard.entity
+ * SprintStatus.java
+ * </pre>
+ *
+ * @author  : minco
+ * @date    : 2023. 12. 26. 오후 5:44:37
+ * @desc    :
+ * @version : x.x
+ */
+public enum SprintStatus {
+	ISSUED, IN_PROGRESS, FINISHED
+}
